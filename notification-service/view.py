7563f1a4e93497eee91edfa9ae453130345036bc
Tony Fiedler <tony.fiedler@mailbox.org>
2020-12-02 21:44:54 +0100

    Implement getting notifications by recipient
    Also return proper HTTP status codes on the different requests.

--- notification-service/view.py
+++ notification-service/view.py
@@ -9,19 +9,37 @@ import json
 @app.route('/notification', methods=['GET', 'POST'])
 def notification():
     """
-    Return all notifications using JSON format on a GET request.
-    Create a new notification object and send the message. Return this object using JSON format on a POST request.
-    TODO: Get requests by recipient
+    On a GET request:
+      Return notifications using JSON format.
+      If recipient is set return all notifications for this recipient.
+    On a POST request:
+      Create a new notification object and send the message via the Telegram bot.
     """
+    status_code = 404
     if request.method == 'GET':
-        notifications = Notification.query.all()
+        recipient = None
+        try:
+            recipient = request.args.get('recipient')
+        except KeyError:
+            pass
+
+        if recipient:
+            # Get messages from a specific recipient
+            notifications = Notification.query.filter_by(recipient=recipient).all()
+            if len(notifications) > 0:  # There might be no notification available by this user
+                status_code = 200
+        else:
+            # Get all messages
+            notifications = Notification.query.all()
+            # Use always code 200 because this is the general endpoint for getting notifications
+            status_code = 200
     else:
         try:
             channel = str(request.form['channel'])
             recipient = str(request.form['recipient'])
             msg = str(request.form['msg'])
         except KeyError:
-            return Response('Required arguments: channel, recipient, msg!', status=400)
+            return Response('Required arguments: channel, recipient, msg!', mimetype='application/json', status=400)
 
         telegram = ChannelTelegram()
         telegram_response, human_readable_error_msg = telegram.send_message(chat_id=recipient, msg=msg)
@@ -37,8 +55,9 @@ def notification():
 
         db.session.add(n)
         db.session.commit()
-        notifications = [n]
-    return Response(json.dumps(notifications, cls=ModelJSONEncoder), mimetype='application/json')
+        notifications = n
+        status_code = 201
+    return Response(json.dumps(notifications, cls=ModelJSONEncoder), mimetype='application/json', status=status_code)
 
 
 @app.route('/notification/<notification_id>')
@@ -46,6 +65,9 @@ def get_notification(notification_id=None):
     """
     Return a notification by id using JSON format.
     """
+    status_code = 404
     n = Notification.query.filter_by(id=notification_id).first()
+    if n:
+        status_code = 200
     json_string = json.dumps(n, cls=ModelJSONEncoder)
-    return Response(json_string, mimetype='application/json')
+    return Response(json_string, mimetype='application/json', status=status_code)
